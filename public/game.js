754e18c5479af96d60c67eb0549306a31279a8d9
YungLonk <amadeoc72@gmail.com>
2020-03-07 13:34:39 -0500

    moved to public folder

---
+++ public/game.js
@@ -0,0 +1,10 @@
+let trash = [];
+let P1 = [];
+let P2 =[];
+let P3 = [];
+let P4 = [];
+
+function start() {
+    // Subtract 7 Copper and 3 Estates from supply piles, and add to player arrays as necessary.
+    // Shuffle.S
+}
